a0776d31e633a154eaf3fb03d7f2ed83e1feb9ef
DDoudine <amandinekemp@gmail.com>
2024-01-27 16:42:56 +0100

    Remove commented code for formatting alphabet characters

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 for ascii_value in range(97, 123):
-    #"{:c}".format(ascii_value) est utilisée pour formater le nombre comme un caractère (lettre).
     print("{:c}".format(ascii_value), end="")
