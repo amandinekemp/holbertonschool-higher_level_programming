39cace1611fb9f4a883d0e6533403f226467363c
DDoudine <amandinekemp@gmail.com>
2024-01-28 16:28:16 +0100

    Refactor infinite_add.py to improve readability and handle empty arguments

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-result = sum(int(arg) for arg in sys.argv[1:])
-
-print(result)
+    total = 0
+    if len(sys.argv) > 1:
+        for i in range(1, (len(sys.argv))):
+            total += int(sys.argv[i])
+    print(total)
