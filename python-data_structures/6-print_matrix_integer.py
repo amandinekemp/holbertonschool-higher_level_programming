94f86096bd7fabd683f9e692494daba609e14d8f
DDoudine <amandinekemp@gmail.com>
2024-01-30 10:25:57 +0100

    Refactor print_matrix_integer function to improve readability

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,10 +4,14 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         formatted_row = []
         
-        for element in row:
-            formatted_element = "{:d}".format(element)
+        for col in row:
+            formatted_element = "{:d}".format(col)
+            
+            if col != row[-1]:
+                formatted_element += " "
+            
             formatted_row.append(formatted_element)
         
-        row_string = " ".join(formatted_row)
+        row_string = "".join(formatted_row)
         
         print(row_string)
