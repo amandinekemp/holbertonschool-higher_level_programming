001a59ef59e794aa6ef0f0882d77f44eda73ad83
DDoudine <amandinekemp@gmail.com>
2024-01-30 10:21:02 +0100

    Refactor add_tuple function to improve readability and maintainability

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,9 +3,8 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a += (0, 0)
-    tuple_b += (0, 0)
-    
-    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+
+    result = (a[0] + b[0], a[1] + b[1])
     return result
