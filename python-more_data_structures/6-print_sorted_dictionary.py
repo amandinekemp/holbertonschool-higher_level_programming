056f8b2c13f9afbe020fce8f9d01b7c06dcc6746
DDoudine <amandinekemp@gmail.com>
2024-01-31 16:03:54 +0100

    Refactor update_dictionary function to print_sorted_dictionary

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-    return a_dictionary
+def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is not None:
+        for key in sorted(a_dictionary):
+            print("{}: {}".format(key, a_dictionary[key]))
