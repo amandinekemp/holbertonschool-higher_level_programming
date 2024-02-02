7cb1bb2a02c5c4e8a368fbde6fe5892aeb7dd497
DDoudine <amandinekemp@gmail.com>
2024-02-02 13:58:01 +0100

    ✨ Fix index variable in list_division function

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -4,7 +4,7 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for index in range(list_length):
         try:
-            result = my_list_1[i] / my_list_2[i]
+            result = my_list_1[index] / my_list_2[index]
         except (TypeError, ValueError):
             print("wrong type")
             result = 0
