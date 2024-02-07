f143d939aaf6799c8f73c207e98c23e1bdbaf0ab
DDoudine <amandinekemp@gmail.com>
2024-02-07 11:05:08 +0100

    Add Square class with size attribute and methods

---
+++ python-classes/4-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Module defining a Square class with a size attribute"""
+
+class Square:
+    """Square class representing a geometric square with a size attribute"""
+
+    def __init__(self, size=0):
+        """Initializes a square instance with an optional size
+
+        Args:
+        	size (int): The size of the square, defaults to 0
+
+        Raises:
+        	TypeError: If the size is not an integer
+        	ValueError: If the size is less than 0
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Gets the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square
+
+        Args:
+        	value (int): The new size of the square
+
+        Raises:
+        	TypeError: If the value is not an integer
+       		ValueError: If the value is less than 0
+        """
+
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculates and returns the current square's area"""
+        return self.__size ** 2
