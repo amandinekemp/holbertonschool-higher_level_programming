531a071c4b763ed7f07e006c843a40461b200297
DDoudine <amandinekemp@gmail.com>
2024-02-08 10:42:06 +0100

    Refactor Square class constructor and add area method

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,18 +3,21 @@
 
 
 class Square:
+
     """Square class representing a geometric square with a size attribute"""
 
     def __init__(self, size=0):
+
         """Initializes a square instance with an optional size
 
         Args:
             size (int): Size of the square. Defaults to 0
-        
+
         Raises:
             TypeError: If size is not an integer
-        	ValueError: If size is less than 0
+            ValueError: If size is less than 0
         """
+
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -23,5 +26,7 @@ class Square:
             self.__size = size
 
     def area(self):
+
         """Calculates and returns the current square's area"""
+
         return self.__size ** 2
