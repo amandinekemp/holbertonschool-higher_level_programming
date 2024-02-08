0f5975419535eee98b5df21aa7b6a7a1dff03906
DDoudine <amandinekemp@gmail.com>
2024-02-08 10:53:42 +0100

    Refactor Square class initialization

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,15 +8,7 @@ class Square:
     """Square class representing a geometric square with a size attribute"""
 
     def __init__(self, size=0):
-        """Initializes a square instance with an optional size
-
-        Args:
-            size (int): Size of the square. Defaults to 0
-
-        Raises:
-            TypeError: If size is not an integer
-            ValueError: If size is less than 0
-        """
+        """Initializes a square instance with an optional size"""
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
