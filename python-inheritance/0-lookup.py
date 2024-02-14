4eeef82d37c3ecf6525e1677fd03fcfa9bacc43b
DDoudine <amandinekemp@gmail.com>
2024-02-14 10:52:19 +0100

    Fix pycodestyle

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 """Returns list of object's attributes and methods"""
 
+
 def lookup(obj):
-	"""returns the list of available attributes and methods of an object"""
-	return dir(obj)
+    """Returns list of object's attributes and methods"""
+    return dir(obj)
