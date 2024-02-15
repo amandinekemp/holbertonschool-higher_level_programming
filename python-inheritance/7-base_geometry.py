d7c62e1cc2842efac2ac3597c4585b89245bf4c3
DDoudine <amandinekemp@gmail.com>
2024-02-15 16:02:49 +0100

    Refactor integer_validator method in BaseGeometry class

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -8,11 +8,9 @@ class BaseGeometry:
     def area(self):
         """Raises an Exception with message 'area() is not implemented'."""
         raise Exception("area() is not implemented")
-
     def integer_validator(self, name, value):
         """Validates value."""
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
-
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
