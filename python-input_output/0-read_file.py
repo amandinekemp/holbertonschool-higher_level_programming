7f41a3921031114605a1dd1e7d03a600aefb2731
DDoudine <amandinekemp@gmail.com>
2024-02-17 09:53:58 +0100

    Add Python script to read a file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Reading a file"""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF-8) and prints it to stdout."""
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end="")
