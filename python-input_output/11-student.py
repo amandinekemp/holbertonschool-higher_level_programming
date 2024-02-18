19e81c08fe74780d092f7d999ca8e78dc41c424b
DDoudine <amandinekemp@gmail.com>
2024-02-18 09:38:22 +0100

    Add Student class with methods to_json and reload_from_json

---
+++ python-input_output/11-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Defines a Student class."""
+
+
+class Student:
+        """Represents a student."""
+
+        def __init__(self, first_name, last_name, age):
+            """Initializes the student.
+
+            Args:
+                first_name (str): The first name of the student.
+                last_name (str): The last name of the student.
+                age (int): The age of the student.
+            """
+            self.first_name = first_name
+            self.last_name = last_name
+            self.age = age
+
+        def to_json(self, attrs=None):
+            """Returns a dictionary representation of a Student instance.
+
+            Args:
+                attrs (list): A list of attributes to retrieve.
+
+            Returns:
+                dict: A dictionary representation of the Student instance.
+            """
+            if attrs is None:
+                return self.__dict__
+            else:
+                dict = {}
+                for key in attrs:
+                    if key in self.__dict__:
+                        dict[key] = self.__dict__[key]
+                return dict
+
+        def reload_from_json(self, json):
+            """Replaces all attributes of the Student instance.
+
+            Args:
+                json (dict): A dictionary of attributes to replace.
+            """
+            for key, value in json.items():
+                self.__dict__[key] = value
