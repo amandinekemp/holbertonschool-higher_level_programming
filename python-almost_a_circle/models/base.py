b21cdb455d45f19cfd636c9df1fadcf0945509d7
DDoudine <amandinekemp@gmail.com>
2024-02-21 14:02:34 +0100

    Ajouter la classe de base avec l'initialisation et l'incrémentation de l'ID

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Base class tests"""
+
+import json
+
+
+class Base:
+	""""Defines a Base class"""
+	__nb_objets = 0
+
+	def __init__(self, id=Nome):
+		""" Initializes the Base class
+
+		Args:
+			id (int): The id of the base. Defaults to None.
+		"""
+		if id is not Nome:
+			self.id = id
+		else:
+			Base.__nb_objets += 1
+			self.id = Base.__nb_objets
