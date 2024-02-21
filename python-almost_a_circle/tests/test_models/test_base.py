d205808b94ed17a3002403af28ca84b15a390892
DDoudine <amandinekemp@gmail.com>
2024-02-21 14:40:26 +0100

    Add tests for Base

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -8,13 +8,36 @@ class test_Base(unittest.TestCase):
     """Tests for the Base class."""
 
     def test_id_none(self):
-        """Sending no id"""
         b = Base()
         self.assertEqual(1, b.id)
-        
-    def test_passed_id(self):
-        b1 = Base(89)
-        self.assertEqual(b1.id, 89)
+
+    def test_id(self):
+        b = Base(50)
+        self.assertEqual(50, b.id)
+
+    def test_id_zero(self):
+        b = Base(0)
+        self.assertEqual(0, b.id)
+
+    def test_id_negative(self):
+        b = Base(-20)
+        self.assertEqual(-20, b.id)
+
+    def test_id_string(self):
+        b = Base("Betty")
+        self.assertEqual("Betty", b.id)
+
+    def test_id_list(self):
+        b = Base([1, 2, 3])
+        self.assertEqual([1, 2, 3], b.id)
+
+    def test_id_dict(self):
+        b = Base({"id": 109})
+        self.assertEqual({"id": 109}, b.id)
+
+    def test_id_tuple(self):
+        b = Base((8,))
+        self.assertEqual((8,), b.id)
 
 
 if __name__ == '__main__':
