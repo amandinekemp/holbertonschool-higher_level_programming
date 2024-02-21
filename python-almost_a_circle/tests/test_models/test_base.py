cb0e2cfc1dc0f155b1b9f9625aa66149a35adde5
DDoudine <amandinekemp@gmail.com>
2024-02-21 14:26:52 +0100

    Add test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,18 +1,20 @@
-#!/usr/bin/python3
+"""Defines test cases for the Base class."""
 
 import unittest, json
 from models.base import Base
 
-"""Defines test cases for the Base class."""
-
 
 class TestBase(unittest.TestCase):
-	"""Tests for the Base class."""
+    """Tests for the Base class."""
 
     def test_id_none(self):
         """Sending no id"""
         b = Base()
         self.assertEqual(1, b.id)
+        
+    def test_passed_id(self):
+        b1 = Base(89)
+        self.assertEqual(b1.id, 89)
 
 
 if __name__ == '__main__':
