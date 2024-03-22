5105af8f72a05e60c250209b406864aa971f3b99
DDoudine <amandinekemp@gmail.com>
2024-03-22 14:31:41 +0100

    Fix print

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,12 +6,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 if __name__ == '__main__':
-	engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-		argv[1], argv[2], argv[3]))
-	
-	Base.metadata.create_all(bind=engine)
-	session = Session(engine)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]
+    ))
 
-	for row in session.query(State).order_by(State.id):
-		print(row.id, end="")
-		print(": ", row.name)
+    Base.metadata.create_all(bind=engine)
+    session = Session(engine)
+
+    for row in session.query(State).order_by(State.id):
+        print(row.id, end="")
+        print(": " + row.name)
