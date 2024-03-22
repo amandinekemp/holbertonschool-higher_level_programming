dca781c05c5b8024f9215e820753bc8c95b05504
DDoudine <amandinekemp@gmail.com>
2024-03-22 11:07:50 +0100

    Fix error in if

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
 
-import MySQLdb
 import sys
+import MySQLdb
 
-if __name__ == '__main_':
-	db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-	cursor = db.cursor()
-	cursor.execute("SELECT * FROM states WHERE name LIKE 'N%';")
-	results = cursor.fetchall()
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE NAME LIKE BINARY 'N%';")
+    result = cursor.fetchall()
 
-	for row in result:
-		print(row)
+    for row in result:
+        print(row)
