f3cf55eb48c363b58932d457668adbc91c466545
DDoudine <amandinekemp@gmail.com>
2024-03-22 14:29:37 +0100

    Add All states via SQLAlchemy

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""""a script that lists all State objects from the database hbtn_0e_6_usa"""
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == '__main__':
+	engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+		argv[1], argv[2], argv[3]))
+	
+	Base.metadata.create_all(bind=engine)
+	session = Session(engine)
+
+	for row in session.query(State).order_by(State.id):
+		print(row.id, end="")
+		print(": ", row.name)
