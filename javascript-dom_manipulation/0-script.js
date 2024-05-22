a997b04f2e2fb821c73b823497f4c4b405c227bc
DDoudine <amandinekemp@gmail.com>
2024-05-22 14:55:44 +0200

    Set header text color to red

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,5 @@
+// Select the header element from the HTML document
+const header = document.querySelector('header');
+
+// Set the color of the header text to red (#FF0000)
+header.style.color = '#FF0000';
