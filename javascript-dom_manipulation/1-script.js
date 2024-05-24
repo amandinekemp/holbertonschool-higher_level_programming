7f2c1bf8ba1bc3db976e069fab890bcd796c50b2
DDoudine <amandinekemp@gmail.com>
2024-05-24 09:40:45 +0200

    Change order for consts

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,8 +1,8 @@
-// Select header element
-const header = document.querySelector('header');
-
 // Select element with ID "red_header"
 const redHeader = document.querySelector('#red_header');
+
+// Select header element
+const header = document.querySelector('header');
 
 // Add click event listener to redHeader element
 redHeader.addEventListener('click', () => {
